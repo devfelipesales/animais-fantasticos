7e4e00af80169f285acbf8bb5bee512edc355e08
Felipe Sales <felipefsp.dev@gmail.com>
2022-12-21 20:55:50 -0300

    refatorar dropdown menu

--- js/module/dropdown-menu.js
+++ js/module/dropdown-menu.js
@@ -1,59 +1,35 @@
 import outsideClick from './outsideclick.js';
 
-export default function initDropDownMenu() {
-  // Caso haja mais de um dropdown no site
-  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+export default class DropDownMenu {
+  constructor(dropdownMenus) {
+    this.dropdownMenus = document.querySelectorAll(dropdownMenus);
 
-  if (!dropdownMenus) {
-    return;
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  dropdownMenus.forEach((item) => {
-    // Click && TouchStart -> Otimizando pra Mobile. Basicamente funcionam da mesma forma, porém o Click é um pouco mais lento que o touchstart
-    //
-    item.addEventListener('click', handleClick);
-    item.addEventListener('touchstart', handleClick);
-  });
-
-  function handleClick(event) {
+  // Ativa o dropdownmenu e adiciona a função que observa o click fora dele
+  handleClick(event) {
+    const element = event.currentTarget;
     event.preventDefault();
-    this.classList.add('ativo');
+    element.classList.add('ativo');
     // this = elemento LI com [data-dropdown]
-    outsideClick(this, () => {
-      this.classList.remove('ativo');
+    outsideClick(element, () => {
+      element.classList.remove('ativo');
     });
   }
-}
 
-/* CÓDIGO REFATORADO PARA UTILIZAR EM OUTRO MOMENTO, PORÉM PODE-SE DESCOMENTAR PARA MELHOR ENTENDIMENTO
-
-// element = elemento LI com [data-dropdown]
-function outsideClick(element, callback) {
-  const html = document.documentElement;
-  const outside = 'data-outside';
-
-  // Executado pelo Event Bubble padrão do JS
-  if (!element.hasAttribute(outside)) {
-    // Adiciona os eventos no html document
-    html.addEventListener('click', handleOutsideClick);
-    html.addEventListener('touchstart', handleOutsideClick);
-    // atribui o data-outside para não ficar adicionando o mesmo evento em todo click.
-    element.setAttribute(outside, '');
+  addDropdownMenusEvent() {
+    this.dropdownMenus.forEach((item) => {
+      // Click && TouchStart -> Otimizando pra Mobile. Basicamente funcionam da mesma forma, porém o Click é um pouco mais lento que o touchstart
+      item.addEventListener('click', this.handleClick);
+      item.addEventListener('touchstart', this.handleClick);
+    });
   }
 
-  // Só quero que a função seja criada, quando o OutsideClick for ativado
-  // Retornando pelo Bubble(ativação das funções dos elementos pai), chegará no HTML e será verificado que tem uma função e click/touchstart e ela será ativada
-  function handleOutsideClick(event) {
-    // verifica se onde está sendo clicado não é um elemento que compõe o dropdown. Ou seja, se for um dos filhos/estiver dentro do li[data-dropdown], mantém o menu aberto, caso contrário está clicando fora, então remove a classe chamado a função de callback
-    // console.log(event.target);
-    // console.log(element);
-    if (!element.contains(event.target)) {
-      // executa a função passada por parametro, que é a de remover a classe ativo. Também remove os eventos
-      callback();
-      html.removeEventListener('click', handleOutsideClick);
-      html.removeEventListener('touchstart', handleOutsideClick);
-      element.removeAttribute(outside);
+  init() {
+    if (this.dropdownMenus.length) {
+      this.addDropdownMenusEvent();
     }
+    return this;
   }
 }
-*/
